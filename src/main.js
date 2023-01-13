7e2d8fbbbc7c44f3edc33f916cdbfe9f39f8b19d
ryu-rhee <ryu-rhee@proton.me>
2023-01-14 03:08:37 +0800

    feat: detail geometry and light

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import './index.css'
 const scene = new THREE.Scene()
 const sizes = { width: window.innerWidth, height: window.innerHeight }
 
-const light = new THREE.PointLight(0xffffff, 1, 100)
+const light = new THREE.PointLight(0xffffff, 1.25, 100)
 light.position.set(0, 10, 10)
 scene.add(light)
 
 const geometry = new THREE.SphereGeometry(5, 64, 32)
-const material = new THREE.MeshStandardMaterial({ color: 0x9496ff })
+const material = new THREE.MeshStandardMaterial({
+  color: 0x9496ff,
+  wireframe: true
+})
 const sphere = new THREE.Mesh(geometry, material)
 scene.add(sphere)
 
