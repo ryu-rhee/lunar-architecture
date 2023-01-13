5514e83e2e54753fad80d46b61a1f32c78c0ff98
ryu-rhee <ryu-rhee@proton.me>
2023-01-14 03:26:59 +0800

    feat: update if resized

--- src/main.js
+++ src/main.js
@@ -63,3 +63,11 @@ window.addEventListener('mousemove', e => {
   }
   mouseEffect && randomColor()
 })
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width, sizes.height)
+})
