29a262008cdfa33ac2314e4037a7883f57978c06
ryu-rhee <ryu-rhee@proton.me>
2023-01-14 02:46:47 +0800

    feat: geometry and light

--- src/main.js
+++ src/main.js
@@ -4,15 +4,25 @@ import './index.css'
 const scene = new THREE.Scene()
 const sizes = { width: window.innerWidth, height: window.innerHeight }
 
+const light = new THREE.PointLight(0xffffff, 1, 100)
+light.position.set(0, 10, 10)
+scene.add(light)
+
+const geometry = new THREE.SphereGeometry(5, 64, 32)
+const material = new THREE.MeshStandardMaterial({ color: 0x9496ff })
+const sphere = new THREE.Mesh(geometry, material)
+scene.add(sphere)
+
 const camera = new THREE.PerspectiveCamera(
   45,
   sizes.width / sizes.height,
   1,
   1000
 )
+camera.position.z = 20
 scene.add(camera)
 
 const renderer = new THREE.WebGLRenderer()
-renderer.render(scene, camera)
 renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
 document.body.appendChild(renderer.domElement)
