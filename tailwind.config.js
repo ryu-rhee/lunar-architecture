f32593350aa316f88b180df68470ec1239af3454
ryu-rhee <ryu-rhee@proton.me>
2023-01-14 05:06:57 +0800

    feat: container and others

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
   content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
-    extend: {}
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '3rem',
+        sm: '5rem',
+        md: '6rem',
+        lg: '7rem',
+        xl: '8rem',
+        '2xl': '9rem'
+      }
+    },
+    extend: {
+      fontFamily: {
+        cursive: ['Lobster']
+      },
+      colors: {
+        primary: '#9496ff'
+      }
+    }
   },
   plugins: []
 }
