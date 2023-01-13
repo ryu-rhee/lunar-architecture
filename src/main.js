e5bcf7fb140ecd3355ac40be5ccacecf9efe7cde
ryu-rhee <ryu-rhee@proton.me>
2023-01-14 02:35:42 +0800

    feat: render canvas

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
+import * as THREE from 'three'
 import './index.css'
+
+const scene = new THREE.Scene()
+const sizes = { width: window.innerWidth, height: window.innerHeight }
+
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  1,
+  1000
+)
+scene.add(camera)
+
+const renderer = new THREE.WebGLRenderer()
+renderer.render(scene, camera)
+renderer.setSize(sizes.width, sizes.height)
+document.body.appendChild(renderer.domElement)
